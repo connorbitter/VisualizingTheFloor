c21fba61b468745944eb763f0307149cdd256bf9
Connor Bitter <connor.bitter@gmail.com>
2017-11-10 11:41:22 -0500

    initial template

---
+++ js/main.js
@@ -0,0 +1,71 @@
+
+
+// Function to convert date objects to strings or reverse
+var dateFormatter = d3.timeFormat("%Y-%m-%d");
+var dateParser = d3.timeParse("%Y-%m-%d");
+
+
+// (1) Load data asynchronously
+queue()
+	.defer(d3.json,"data/perDayData.json")
+	.defer(d3.json,"data/myWorldFields.json")
+	.await(createVis);
+
+
+function createVis(error, perDayData, metaData){
+	if(error) { console.log(error); }
+
+	// (2) Make our data look nicer and more useful
+	allData = perDayData.map(function (d) {
+		
+		var result = {
+			time: dateParser(d.day),
+			count: +d["count(*)"] + 1
+		};
+
+		// Convert votes for the 15 priorities from key-value format into one single array (for each day)
+		result.priorities = d3.range(0,15).map(function(counter){
+			return d["sum(p"+counter+")"]
+		});
+		// [d["sum(p0)"], d["sum(p1)"], d["sum(p2)"],...]
+		// Example: [10,200,500,... ]
+
+		// Create an array of values for age 0 - 99
+		result.ages = d3.range(0,99).map(function(){
+			return 0;
+		});
+
+		// Insert the votes in the newly created array 'result.ages'
+		d.age.forEach(function(a){
+			if(a.age < 100){
+				result.ages[a.age] = a["count(*)"];
+			}
+		})
+		
+		return result;
+	});
+
+	console.log(allData);
+	console.log(perDayData);
+	console.log(metaData);
+
+
+	// (3) Create event handler
+	BrushHandler = {};
+
+	ZoomHandler = {};
+
+	// (4) Create visualization instances
+	var countVis = new CountVis("countvis", allData);
+	var ageVis = new AgeVis("agevis", perDayData);
+	var prioVis = new PrioVis("priovis", perDayData, metaData);
+
+	// (5) Bind event handler
+	
+	$(BrushHandler).bind("selectionChanged", function(event, rangeStart, rangeEnd){
+			countVis.onSelectionChange(rangeStart, rangeEnd);
+	    ageVis.onSelectionChange(rangeStart, rangeEnd);
+	    prioVis.onSelectionChange(rangeStart, rangeEnd);
+	});
+
+}

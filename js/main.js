ea98bdc70ffa8c41597cf77c50e91209c0d21420
Ryan Plunkett <Ryan@Ryans-MacBook-Air-3.local>
2017-11-12 18:32:48 -0500

    Stuck trying to merge data

--- js/main.js
+++ js/main.js
@@ -1,71 +1,93 @@
-
-
-// Function to convert date objects to strings or reverse
-var dateFormatter = d3.timeFormat("%Y-%m-%d");
-var dateParser = d3.timeParse("%Y-%m-%d");
-
-
 // (1) Load data asynchronously
 queue()
-	.defer(d3.json,"data/perDayData.json")
-	.defer(d3.json,"data/myWorldFields.json")
+	.defer(d3.json,"data/shot_locations.json")
+	.defer(d3.csv,"data/defender_stats.csv")
 	.await(createVis);
 
 
-function createVis(error, perDayData, metaData){
-	if(error) { console.log(error); }
+function createVis(error, shot_locations, defender_stats){
+  // Separate shots and league averages
+  var shots = shot_locations.resultSets[0].rowSet;
+  var averages = shot_locations.resultSets[1].rowSet;
 
-	// (2) Make our data look nicer and more useful
-	allData = perDayData.map(function (d) {
-		
-		var result = {
-			time: dateParser(d.day),
-			count: +d["count(*)"] + 1
-		};
+  // Process shots
+  shots.forEach(function(d) {
+    d[1] = +d[1];
+    d[21] = +d[21];
+    d.GRID_TYPE = d[0];
+    d.GAME_ID = d[1];
+    d.GAME_EVENT_ID = d[2];
+    d.PLAYER_ID = d[3];
+    d.PLAYER_NAME = d[4];
+    d.TEAM_ID = d[5];
+    d.TEAM_NAME = d[6];
+    d.PERIOD = d[7];
+    d.MINUTES_REMAINING = d[8];
+    d.SECONDS_REMAINING = d[9];
+    d.EVENT_TYPE = d[10];
+    d.ACTION_TYPE = d[11];
+    d.SHOT_TYPE = d[12];
+    d.SHOT_ZONE_BASIC = d[13];
+    d.SHOT_ZONE_AREA = d[14];
+    d.SHOT_ZONE_RANGE = d[15];
+    d.SHOT_DISTANCE = d[16];
+    d.LOC_X = d[17];
+    d.LOC_Y = d[18];
+    d.SHOT_ATTEMPTED_FLAG = d[19];
+    d.SHOT_MADE_FLAG = d[20];
+    d.GAME_DATE = d[21];
+    d.HTM = d[22];
+    d.VTM = d[23];
+    for (var key = 0; key < 24; key++) {
+      delete d[key];
+    }
+  });
 
-		// Convert votes for the 15 priorities from key-value format into one single array (for each day)
-		result.priorities = d3.range(0,15).map(function(counter){
-			return d["sum(p"+counter+")"]
-		});
-		// [d["sum(p0)"], d["sum(p1)"], d["sum(p2)"],...]
-		// Example: [10,200,500,... ]
+  // Process defender_stats
+  defender_stats.forEach(function(d) {
+    d.CLOSEST_DEFENDER_PLAYER_ID = +d.CLOSEST_DEFENDER_PLAYER_ID;
+    d.CLOSE_DEF_DIST = +d.CLOSE_DEF_DIST;
+    d.DRIBBLES = +d.DRIBBLES;
+    d.FGM = +d.FGM;
+    d.FINAL_MARGIN = +d.FINAL_MARGIN;
+    d.GAME_ID = +d.GAME_ID;
+    d.PERIOD = +d.PERIOD;
+    d.PTS = +d.PTS;
+    d.PTS_TYPE = +d.PTS_TYPE;
+    d.SHOT_CLOCK = +d.SHOT_CLOCK;
+    d.SHOT_DIST = +d.SHOT_DIST;
+    d.SHOT_NUMBER = +d.SHOT_NUMBER;
+    d.TOUCH_TIME = +d.TOUCH_TIME;
+    d.PLAYER_ID = +d.player_id;
+    delete d.player_id;
+    d.MINUTES_REMAINING = +d.GAME_CLOCK.split(":")[0];
+    d.SECONDS_REMAINING = +d.GAME_CLOCK.split(":")[1];
+  });
 
-		// Create an array of values for age 0 - 99
-		result.ages = d3.range(0,99).map(function(){
-			return 0;
-		});
+  // Log everything
+  console.log(shots);
+  console.log(defender_stats);
 
-		// Insert the votes in the newly created array 'result.ages'
-		d.age.forEach(function(a){
-			if(a.age < 100){
-				result.ages[a.age] = a["count(*)"];
-			}
-		})
-		
-		return result;
-	});
+  // Initialize counter to track progress and array to store data
+  var ctr = 0;
+  var mergedData = [];
 
-	console.log(allData);
-	console.log(perDayData);
-	console.log(metaData);
+  // Check for matches between data and merge
+  for (var i = 0; i < shots.length; i++) {
+    var filtered = defender_stats.filter(function(d) {
+    return (d.PLAYER_ID == shots[i].PLAYER_ID && d.GAME_ID ==
+            shots[i].GAME_ID && d.PERIOD == shots[i].PERIOD &&
+            d.MINUTES_REMAINING == shots[i].MINUTES_REMAINING  &&
+            Math.abs(d.SECONDS_REMAINING - shots[i].SECONDS_REMAINING) <= 5);
+    });
+    if (filtered.length == 1) {
+      ctr += 1;
+      console.log(ctr);
+      mergedData.push($.extend(filtered[0], shots[i]));
+    };
+  };
 
-
-	// (3) Create event handler
-	BrushHandler = {};
-
-	ZoomHandler = {};
-
-	// (4) Create visualization instances
-	var countVis = new CountVis("countvis", allData);
-	var ageVis = new AgeVis("agevis", perDayData);
-	var prioVis = new PrioVis("priovis", perDayData, metaData);
-
-	// (5) Bind event handler
-	
-	$(BrushHandler).bind("selectionChanged", function(event, rangeStart, rangeEnd){
-			countVis.onSelectionChange(rangeStart, rangeEnd);
-	    ageVis.onSelectionChange(rangeStart, rangeEnd);
-	    prioVis.onSelectionChange(rangeStart, rangeEnd);
-	});
-
+  // Create link to download JSON
+  var dataString = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(mergedData));
+  $("<a href='" + dataString + "' download='data.json'>download JSON</a>").appendTo("#download-container");
 }

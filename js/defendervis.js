93503eb6679b92572e134f7fe335422a3b399760
Brian Ge <briange1997@gmail.com>
2017-11-13 21:32:22 -0500

    Refactored js files, filtered data to be 3 point only

---
+++ js/defendervis.js
@@ -0,0 +1,209 @@
+
+/*
+ * DefenderVis - Object constructor function
+ * @param _parentElement 	-- the HTML element in which to draw the visualization
+ * @param _data						-- the actual data
+ */
+
+DefenderVis = function(_parentElement, _data){
+	this.parentElement = _parentElement;
+	this.data = _data;
+	this.filteredData = this.data;
+
+	this.initVis();
+}
+
+/*
+ * Initialize visualization (static content, e.g. SVG area or axes)
+ */
+
+DefenderVis.prototype.initVis = function(){
+	var vis = this;
+
+	vis.margin = {top: 20, right: 20, bottom: 40, left: 80};
+
+	vis.width = $("#" + vis.parentElement).width() - vis.margin.left - vis.margin.right,
+		vis.height = 300 - vis.margin.top - vis.margin.bottom;
+
+	// SVG drawing area
+	vis.svg = d3.select("#" + vis.parentElement).append("svg")
+		.attr("width", vis.width + vis.margin.left + vis.margin.right)
+		.attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+		.append("g")
+		.attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+
+	vis.extent = d3.extent(vis.data, function(d){return d.CLOSE_DEF_DIST});
+
+	// Scales
+	vis.x = d3.scaleLinear()
+			.range([0, vis.width]);
+
+	vis.y = d3.scaleLinear()
+		.range([vis.height, 0]);
+
+	// Axes
+	vis.xAxis = d3.axisBottom();
+	vis.yAxis = d3.axisLeft();
+
+	// Draw Axes
+	vis.svg.append("g")
+	  .attr("class", "axis x-axis")
+	  .attr("transform", "translate(0," + vis.height + ")")
+
+	vis.svg.append("g")
+	  .attr("class", "axis y-axis")
+
+	// Label Axes
+	vis.svg.append("text")
+    .attr("text-anchor", "middle")
+    .attr("transform", "translate("+ (-vis.margin.left / 1.5) + "," + (vis.height / 2) + ")rotate(-90)")
+    .text("Shot Percentage")
+
+	vis.svg.append("text")
+    .attr("text-anchor", "middle")
+    .attr("transform", "translate("+ (vis.width / 2) + "," + (vis.height - (vis.margin.bottom / 4)) + ")")
+    .text("Defender Distance (ft)");
+
+	// Line
+	vis.line = d3.line();
+	vis.svg.append("path")
+	  .attr("class", "line");
+
+	// (Filter, aggregate, modify data)
+	vis.initData();
+}
+
+DefenderVis.prototype.initData = function(){
+	var vis = this;
+
+	vis.filteredData = vis.filteredData.filter(function(element) {
+		return element.PTS_TYPE == 3;
+	});
+
+	// Store shot percentages by distance (min. 10 shots)
+	vis.percentages = {};
+
+	for (i = vis.extent[0]; i < vis.extent[1] + 0.1; i += 0.1) {
+
+		// Fix floating point error
+		var i_r = (Math.round(i * 10) / 10);
+
+		// Counters
+		var shots_total = 0;
+		var shots_made = 0;
+
+		// Loop through all shots
+		for (j = 0; j < vis.filteredData.length; j++) {
+			if (vis.filteredData[j]['CLOSE_DEF_DIST'] == i_r) {
+				shots_total++;
+				shots_made += vis.filteredData[j]['FGM']
+			}
+		}
+
+		if (shots_total >= 10) 
+			vis.percentages[i_r.toFixed(1)] = (shots_made / shots_total);
+	}
+
+	vis.data = vis.percentages;
+
+	vis.extent = d3.extent(Object.keys(vis.data), function(d) {
+		return +d;
+	});
+
+	// Slider 
+	vis.slider = document.getElementById("def-dist-slider");
+
+	// Create slider based on data
+	noUiSlider.create(vis.slider, {
+		start: vis.extent,
+		step: 0.1,
+		connect: true,
+		tooltips: true,
+		range: {
+			'min': vis.extent[0],
+			'max': vis.extent[1]
+		}});
+
+	vis.slider.noUiSlider.on('update', function(values, handle) {
+		vis.onSelectionChange();
+	});
+}
+
+/*
+ * Data wrangling
+ */
+
+DefenderVis.prototype.wrangleData = function(){
+	var vis = this;
+
+	vis.extent = d3.extent(Object.keys(vis.filteredData), function(d) {
+		return +d;
+	});
+
+	vis.displayData = vis.filteredData;
+
+	// Update the visualization
+	vis.updateVis();
+}
+
+/*
+ * The drawing function
+ */
+
+DefenderVis.prototype.updateVis = function(){
+	var vis = this;
+
+  vis.x.domain(vis.extent);
+  vis.y.domain([0, 1]);
+
+  // Update Axes
+  vis.xAxis.scale(vis.x);
+  vis.yAxis.scale(vis.y).tickFormat(function(d){return (d * 100) + "%"});
+
+  d3.select(".x-axis")
+    .transition()
+    .duration(800)
+    .call(vis.xAxis)
+
+  d3.select(".y-axis")
+    .transition()
+    .duration(800)
+    .call(vis.yAxis)
+
+  // Update Line
+  vis.line
+    .x(function(d) { return vis.x(d); })
+    .y(function(d) { 
+      if (isNaN(vis.displayData[d])) {
+        return vis.y(0);
+      } 
+      else {
+        return vis.y(vis.displayData[d]);
+      }
+    });
+
+  d3.select(".line")
+    .attr("d", vis.line(Object.keys(vis.displayData)))
+    .attr("class", "line")
+    .attr("stroke", "#003DA5")
+    .attr("stroke-width", 2)
+    .attr("fill", "none");
+}
+
+
+DefenderVis.prototype.onSelectionChange = function(){
+	var vis = this;
+
+	// Filter data depending on selected range
+	var sliderRange = vis.slider.noUiSlider.get();
+
+	vis.filteredData = {};
+
+	for (var key in vis.data) {
+		if (+key >= sliderRange[0] && +key <= sliderRange[1]) {
+			vis.filteredData[key] = vis.data[key];
+		}
+	}
+
+	vis.wrangleData();
+}
---
+++ js/wrangle_data.js
@@ -0,0 +1,95 @@
+// (1) Load data asynchronously
+queue()
+	.defer(d3.json,"data/shot_locations.json")
+	.defer(d3.csv,"data/defender_stats.csv")
+	.await(createVis);
+
+
+function createVis(error, shot_locations, defender_stats){
+  // Separate shots and league averages
+  var shots = shot_locations.resultSets[0].rowSet;
+  var averages = shot_locations.resultSets[1].rowSet;
+
+  // Process shots
+  shots.forEach(function(d) {
+    d[1] = +d[1];
+    d[21] = +d[21];
+    d.GRID_TYPE = d[0];
+    d.GAME_ID = d[1];
+    d.GAME_EVENT_ID = d[2];
+    d.PLAYER_ID = d[3];
+    d.PLAYER_NAME = d[4];
+    d.TEAM_ID = d[5];
+    d.TEAM_NAME = d[6];
+    d.PERIOD = d[7];
+    d.MINUTES_REMAINING = d[8];
+    d.SECONDS_REMAINING = d[9];
+    d.EVENT_TYPE = d[10];
+    d.ACTION_TYPE = d[11];
+    d.SHOT_TYPE = d[12];
+    d.SHOT_ZONE_BASIC = d[13];
+    d.SHOT_ZONE_AREA = d[14];
+    d.SHOT_ZONE_RANGE = d[15];
+    d.SHOT_DISTANCE = d[16];
+    d.LOC_X = d[17];
+    d.LOC_Y = d[18];
+    d.SHOT_ATTEMPTED_FLAG = d[19];
+    d.SHOT_MADE_FLAG = d[20];
+    d.GAME_DATE = d[21];
+    d.HTM = d[22];
+    d.VTM = d[23];
+    for (var key = 0; key < 24; key++) {
+      delete d[key];
+    }
+  });
+
+  // Process defender_stats
+  defender_stats.forEach(function(d) {
+    d.CLOSEST_DEFENDER_PLAYER_ID = +d.CLOSEST_DEFENDER_PLAYER_ID;
+    d.CLOSE_DEF_DIST = +d.CLOSE_DEF_DIST;
+    d.DRIBBLES = +d.DRIBBLES;
+    d.FGM = +d.FGM;
+    d.FINAL_MARGIN = +d.FINAL_MARGIN;
+    d.GAME_ID = +d.GAME_ID;
+    d.PERIOD = +d.PERIOD;
+    d.PTS = +d.PTS;
+    d.PTS_TYPE = +d.PTS_TYPE;
+    d.SHOT_CLOCK = +d.SHOT_CLOCK;
+    d.SHOT_DIST = +d.SHOT_DIST;
+    d.SHOT_NUMBER = +d.SHOT_NUMBER;
+    d.TOUCH_TIME = +d.TOUCH_TIME;
+    d.PLAYER_ID = +d.player_id;
+    delete d.player_id;
+    d.MINUTES_REMAINING = +d.GAME_CLOCK.split(":")[0];
+    d.SECONDS_REMAINING = +d.GAME_CLOCK.split(":")[1];
+  });
+
+  // Log everything
+  console.log(shots);
+  console.log(defender_stats);
+
+  // Initialize counter to track progress and array to store data
+  var ctr = 0;
+  var mergedData = [];
+
+  // Check for matches between data and merge
+  for (var i = 0; i < shots.length; i++) {
+    var filtered = defender_stats.filter(function(d) {
+    return (d.PLAYER_ID == shots[i].PLAYER_ID && d.GAME_ID ==
+            shots[i].GAME_ID && d.PERIOD == shots[i].PERIOD &&
+            d.MINUTES_REMAINING == shots[i].MINUTES_REMAINING  &&
+            Math.abs(d.SECONDS_REMAINING - shots[i].SECONDS_REMAINING) <= 5);
+    });
+    if (filtered.length == 1) {
+      ctr += 1;
+      console.log(ctr);
+      mergedData.push($.extend(filtered[0], shots[i]));
+    };
+  };
+  // console.log(mergedData);
+
+  // Create link to download JSON
+  // var dataString = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(mergedData));
+  console.log(JSON.stringify(mergedData));
+  // $("<a href='" + dataString + "' download='data.json'>download JSON</a>").appendTo("#download-container");
+}

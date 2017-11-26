d67ce4eff27e411a1b974ab0f35221ca340ff2b7
Ryan Plunkett <Ryan@Ryans-MacBook-Air-3.local>
2017-11-25 21:53:56 -0500

    Pushing again

--- js/shotchartvis.js
+++ js/shotchartvis.js
@@ -63,7 +63,7 @@ ShotChartVis = function(_parentElement, _data){
 
   vis.svg.append("text")
     .attr("text-anchor", "middle")
-    .attr("transform", "translate("+ (vis.width / 2) + "," + (vis.height - (vis.margin.bottom / 4)) + ")")
+    .attr("transform", "translate("+ (vis.width / 2) + "," + vis.height + ")")
     .text("X Location");
 
   // Title
@@ -74,8 +74,83 @@ ShotChartVis = function(_parentElement, _data){
         .style("font-size", "22px")
         .text("Shot Chart");
 
-  // (Filter, aggregate, modify data)
-  vis.initData();
+  // Get list of all players that recorded a shot
+  var unique = {};
+  var players = [];
+  for (var i=0; i < vis.data.length; i++){
+   if(typeof(unique[vis.data[i].PLAYER_NAME]) == "undefined"){
+    players.push(vis.data[i].PLAYER_NAME);
+   }
+   unique[vis.data[i].PLAYER_NAME] = 0;
+  }
+
+  // Function to sort players by last name
+  function compare(a, b) {
+    var splitA = a.split(" ");
+    var splitB = b.split(" ");
+    var lastA = splitA[splitA.length - 1];
+    var lastB = splitB[splitB.length - 1];
+
+    if (lastA < lastB) return -1;
+    if (lastA > lastB) return 1;
+    return 0;
+  }
+  players = players.sort(compare);
+
+  // Add players as selectbox options
+  var sel = document.getElementById('player-dropdown');
+  for(var i = 0; i < players.length; i++) {
+      var opt = document.createElement('option');
+      opt.innerHTML = players[i];
+      opt.value = players[i];
+      sel.appendChild(opt);
+  }
+
+  // Call next function
+  vis.wrangleData();
 
  }
 
+ ShotChartVis.prototype.wrangleData = function(){
+    var vis = this;
+
+    // Get current player selection
+    vis.dropdown = d3.select("#player-dropdown");
+    vis.selection = vis.dropdown.property("value");
+
+    console.log(vis.selection);
+
+    // Filter by current player
+    vis.filteredData = vis.filteredData.filter(function(element){
+      return element.PLAYER_NAME == vis.selection;
+    });
+
+    vis.displayData = vis.filteredData;
+
+    vis.updateVis();
+ }
+
+ ShotChartVis.prototype.updateVis = function(){
+    var vis = this;
+
+    vis.x.domain(vis.x_extent);
+    vis.y.domain(vis.y_extent);
+
+    var circle = vis.svg
+      .selectAll("circle")
+      .data(vis.displayData);
+    circle.enter()
+      .append("circle")
+      .attr("r", 3)
+      .attr("cx", function(d){
+        return vis.x(d.LOC_X) })
+      .attr("cy", function(d){ return vis.y(d.LOC_Y) })
+      .attr("fill", function(d){
+        if (d.FGM) { return "green" }
+        else { return "red" }
+      });
+      circle.exit().remove();
+
+    // Wrangle again if selection changes
+    //vis.dropdown.on("change", vis.wrangleData());
+ }

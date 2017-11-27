43ab5822357154eb6e4bde4cc18ca5355e1cb98f
Ryan Plunkett <Ryan@Ryans-MacBook-Air-3.local>
2017-11-26 19:16:12 -0500

    Updated data_cleaned.json

--- python/json_clean.py
+++ python/json_clean.py
@@ -1,15 +1,14 @@
 import json
 
-data = json.load(open('data_full.json'))
-print len(data)
+data = json.load(open('data_cleaned.json'))
+print(len(data))
 
-# for element in data: 
+# for element in data:
 #     del element['MATCHUP']
 #     del element['MINUTES_REMAINING']
 #     del element['EVENT_TYPE']
 #     del element['GRID_TYPE']
-#     del element['TEAM_NAME']
-#     del element['player_name']
+#    del element['player_name']
 #     del element['SHOT_DISTANCE']
 #     del element['SHOT_ATTEMPTED_FLAG']
 #     del element['SHOT_MADE_FLAG']
